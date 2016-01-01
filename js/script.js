7f2f044ee0e241fb42c5daa2af247965d70866bb
Mateus Machado <mateus.sousa1856@outlook.com>
2016-01-01 21:43:15 -0200

    Script simples da calculadora.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
+var equacao; 
 
+function preencher(valorBotao){
+	equacao = document.getElementById('visor').value += valorBotao;
+}
+
+function calcular(){
+	var result = eval(equacao);
+	document.getElementById('visor').value = '';
+	document.getElementById('visor').value = result;
+}
+
+function limparPorNum(){
+    var numeros = document.getElementById('visor').value;
+    
+    var str = new String(numeros);
+ 
+    var menosUmNumero = str.substring(0,(str.length - 1));
+
+    document.getElementById('visor').value = menosUmNumero;
+}
+
+function raiz(){
+    var valor = document.getElementById('visor').value;
+
+    var x = Math.sqrt(valor);
+
+    document.getElementById('visor').value = x;
+    
+}
